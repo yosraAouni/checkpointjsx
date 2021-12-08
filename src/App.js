2c6dbcfea529e6a2e12218ae55261aecc07a85d2
yosra <yosaouni@gmail.com>
2021-12-08 21:59:21 +0100

    secondcheck

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
+import myImage from './2.jfif'
+import myVideo from './3.mp4'
 
 function App() {
+  let name="Yosra Ouni"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:"solid 1px black", maxWidth:"100vw"}}>
+
+          <h1 className="title red" >My name : {name}</h1>
+
+          <br/>
+          <div className="pic">
+          <img src={myImage} alt="mypicture" />
+          
+          <img src="./1.jpg" alt="mypicture" style={{width:'25%',height:'10%'}}/>
+          </div>
+
+          </div>
+
+          <video width="320" height="240" controls>
+
+          <source src={myVideo} type="video/mp4"/>
+
+          </video>
+          
     </div>
   );
 }
